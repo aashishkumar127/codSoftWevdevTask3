adf92261ca8913191e21dd5e14bfcbcc41c95e87
Aashish Kumar <158806798+aashishkumar127@users.noreply.github.com>
2024-07-20 15:48:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+const inputField = document.getElementById('input');
+const buttons = document.querySelectorAll('.btn');
+let inputValue = '';
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const buttonText = e.target.textContent;
+    if (buttonText === 'C') {
+      inputValue = '';
+      inputField.value = '';
+    } 
+    else if (buttonText === '%') {
+      inputValue += '%';
+      inputField.value = inputValue;
+    } 
+    else if (buttonText === 'del') {
+      inputValue = inputValue.slice(0, -1);
+      inputField.value = inputValue;
+    }
+    else if (buttonText === '=') {
+        const result = eval(inputValue);
+        inputField.value = result;
+        inputValue = result;
+    } 
+    else {
+      inputValue += buttonText;
+      inputField.value = inputValue;
+    }
+  });
+});
